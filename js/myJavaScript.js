ede30310d2bdd734c8a46f503fd3532a603ec0f8
PatrickBrouder <K00203617@student.lit.ie>
2016-04-02 18:07:25 +0100

    w

--- js/myJavaScript.js
+++ js/myJavaScript.js
@@ -14,9 +14,14 @@ $(document).ready(function() {
 		});
          e.preventDefault();
 	});
-    $('#courseInfo li').click(function(e) {
+   
+     $('#courseInfo a').click(function(e) {
 		var courseFilter= $(this).attr('data-filter');
- 		$('div#courses div').each(function() {
+         $('#courseInfo a.current').removeClass('current');
+        $(this).addClass('current');
+        $('#courseInfo div.current').removeClass('current');
+         $(this).parent().parent().addClass('current');
+         $('div#courses div').each(function() {
     		if($(this).hasClass(courseFilter)) {
       			$(this).fadeIn('slow').removeClass('hidden');
     		} else {
